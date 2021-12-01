2d9a38d96871b27dcdc78ae0a0a0ecf546327786
Shrusti R <shrusti.r@govisetech.com>
2021-12-01 17:02:03 +0530

    CFDR2-5 Updated changes

--- CLOVise/PLM/Outbound/PublishToPLM/CreateProduct.cpp
+++ CLOVise/PLM/Outbound/PublishToPLM/CreateProduct.cpp
@@ -172,7 +172,7 @@ namespace CLOVise
 		ui_colorwayTable->setContextMenuPolicy(Qt::CustomContextMenu); ui_colorwayTable->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 		ui_colorwayTable->horizontalHeader()->setStyleSheet("QHeaderView { font-face: ArialMT; font-size: 10px;background-color:#2D2D2F; color: #FFFFFF; font-weight: bold;}" "QHeaderView::section:horizontal{border: 1px solid #000000;}" "QHeaderView::down-arrow{ image: url(:/CLOVise/PLM/Images/ui_spin_icon_minus_over.svg); width: 18px; height: 18px; color: #0000FF }" " QHeaderView::up-arrow{ image: url(:/CLOVise/PLM/Images/ui_spin_icon_plus_over.svg); width: 18px; height: 18px; color: #0000FF }");
 		ui_colorwayTable->horizontalHeader()->setStretchLastSection(true);
-		ui_colorwayTable->verticalHeader()->setStyleSheet("QHeaderView { color: #FFFFFF;font-weight: bold; background-color:#2D2D2F;}" "QHeaderView::section:vertical{border: 1px solid #000000;}");
+		//ui_colorwayTable->verticalHeader()->setStyleSheet("QHeaderView { color: #FFFFFF;font-weight: bold; background-color:#2D2D2F;}" "QHeaderView::section:vertical{border: 1px solid #000000;}");
 
 		//ui_colorwayTable->verticalHeader()->setStyleSheet("QHeaderView { font-face: ArialMT; font-size: 10px; color: #FFFFFF; min-width: 30px; }""QHeaderView::section:vertical{ background-color:#262628; border-right: 0.5px solid #262628; border-bottom: 0.5px solid #000000; border-top: 0.2px solid #000000; border-left: 0.5px solid #000000; min-width: 30px;}");
 		ui_colorwayTable->setStyleSheet("QTableWidget{ background-color: #262628; border-right: 0px solid #000000; border-top: 0px solid #000000; border-left: 0px solid #000000; font-face: ArialMT; font-size: 12px; color: #FFFFFF; }"
